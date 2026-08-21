f271184b72c7dfef01da24f4545db1e241a34a7a
victor-feitosaa <dev.victorfeitosa@outlook.com>
2026-08-21 16:34:47 -0500

    ADD: new portfolio and hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://victorfeitosa.dev',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
